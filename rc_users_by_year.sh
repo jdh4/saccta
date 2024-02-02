127719c6f51e3d0aaa8c6b217505c5fb817c4436
jdh4 <52128661+jdh4@users.noreply.github.com>
2024-02-01 21:50:31 -0500

    Update rc_users_by_year.sh

--- rc_users_by_year.sh
+++ rc_users_by_year.sh
@@ -5,6 +5,8 @@
 # One should find the number of clusters for given year and then check to see
 # if there are valid jobs associated with each cluster.
 # sacct -L -a -X -n -S 2017-01-01 -E 2017-12-31 -o cluster | sort | uniq
+# One must include the jobid for cpu-hours and gpu-hours to drop the duplicate
+# entries that arise from jobs that span two time windows.
 
 declare -A clusters
 clusters[2017]="della,perseus,tiger,tiger2,tukey"
@@ -26,9 +28,23 @@ do
     sacct -M ${clusters[${YEAR}]} -a -X -n -S ${YEAR}-07-01 -E ${YEAR}-09-30 -o user | sort | uniq >> ${large_users}
     sacct -M ${clusters[${YEAR}]} -a -X -n -S ${YEAR}-10-01 -E ${YEAR}-12-31 -o user | sort | uniq >> ${large_users}
 
+    cpu_hours=cpu_hours_${YEAR}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-01-01 -E ${YEAR}-03-31 -o jobid,cputimeraw >  ${cpu_hours}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-04-01 -E ${YEAR}-06-30 -o jobid,cputimeraw >> ${cpu_hours}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-07-01 -E ${YEAR}-09-30 -o jobid,cputimeraw >> ${cpu_hours}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-10-01 -E ${YEAR}-12-31 -o jobid,cputimeraw >> ${cpu_hours}
+
+    gpu_hours=gpu_hours_${YEAR}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-01-01 -E ${YEAR}-03-31 -o elapsedraw,alloctres,jobid | grep gres/gpu=[1-9] >  ${gpu_hours}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-04-01 -E ${YEAR}-06-30 -o elapsedraw,alloctres,jobid | grep gres/gpu=[1-9] >> ${gpu_hours}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-07-01 -E ${YEAR}-09-30 -o elapsedraw,alloctres,jobid | grep gres/gpu=[1-9] >> ${gpu_hours}
+    sacct -M ${clusters[${YEAR}]} -a -X -n -P -S ${YEAR}-10-01 -E ${YEAR}-12-31 -o elapsedraw,alloctres,jobid | grep gres/gpu=[1-9] >> ${gpu_hours}
+
     printf ${YEAR}" "
-    cat ${large_users} | sort | uniq | wc -l | tr "\n" " "
-    cat ${large_users} adroit_users_${YEAR} | sort | uniq | wc -l | tr "\n" " "
+    cat ${large_users} | sort | uniq | wc -l | tr "\n" ' '
+    cat ${large_users} adroit_users_${YEAR} | sort | uniq | wc -l | tr "\n" ' '
+    cat ${cpu_hours} | sort | uniq | awk -F'|' '{sum += $2} END {print int(sum/3600)}' | tr "\n" ' '
+    cat ${gpu_hours} | sort | uniq | sed -E "s/\|.*gpu=/,/" | awk -F"," '{sum += $1*$2} END {print int(sum/3600)}' | tr "\n" ' '
     printf ${clusters[${YEAR}]}
     echo ""
 done
