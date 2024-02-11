d7b5e0f7eaa536f03f2877c76866bf12a08206ba
jdh4 <52128661+jdh4@users.noreply.github.com>
2024-02-11 12:06:04 -0500

    Added start time correction.

--- cryoem_gpu_usage.py
+++ cryoem_gpu_usage.py
@@ -1,5 +1,6 @@
 #!/usr/licensed/anaconda3/2023.9/bin/python -uB
 
+import os
 import re
 import argparse
 import subprocess
@@ -10,7 +11,7 @@ from email.message import EmailMessage
 import pandas as pd
 
 
-def get_time_window(num_days: int) -> tuple[str, ...]:
+def get_time_window(num_days: int) -> tuple[str, str, int]:
     """Find the start and end dates."""
     today = datetime.now()
     start_date = today - timedelta(days=num_days)
@@ -83,23 +84,38 @@ if __name__ == "__main__":
                         help='Start date is N previous days from today (default: 14)')
     parser.add_argument('--gpus',
                         type=int,
-                        default=168,
+                        default=152,
                         metavar='G',
-                        choices=range(1, 169),
-                        help='Maximum number of GPUs available (default: 168)')
+                        choices=range(1, 153),
+                        help='Maximum number of GPUs available (default: 152)')
     args = parser.parse_args()
+
+    # convert slurm timestamps to seconds
+    os.environ["SLURM_TIME_FORMAT"] = "%s"
 
     clusters = "della"
     start_date, end_date, elapsed_seconds = get_time_window(args.days)
     partitions = "-r cryoem"
-    fields = "alloctres,elapsedraw"
+    fields = "alloctres,elapsedraw,start"
     df = get_data_from_sacct(clusters, start_date, end_date, partitions, fields)
 
-    # clean data
+    # clean elapsedraw field
     df = df[pd.notna(df.elapsedraw)]
     df = df[df.elapsedraw.str.isnumeric()]
     df.elapsedraw = df.elapsedraw.astype("int64")
     df = df[df.elapsedraw > 0]
+    # clean start field
+    df = df[pd.notna(df.start)]
+    df = df[df.start.str.isnumeric()]
+    df.start = df.start.astype("int64")
+
+    # apply correction to only include the usage during the time window
+    correction = True
+    if correction:
+        start_dt = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S")
+        df["secs-from-start"] = df["start"] - start_dt.timestamp()
+        df["secs-from-start"] = df["secs-from-start"].apply(lambda x: x if x < 0 else 0)
+        df["elapsedraw"] = df["elapsedraw"] + df["secs-from-start"]
 
     # add columns
     df["gpus"] = df.alloctres.apply(gpus_per_job)
