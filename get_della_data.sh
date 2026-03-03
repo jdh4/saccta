9ea54a326ffb8f00acc39e8592bee7ff8839c8e0
Jonathan Halverson <52128661+jdh4@users.noreply.github.com>
2026-03-03 14:44:23 -0500

    Refactor SLURM command to use dynamic partitions

--- get_della_data.sh
+++ get_della_data.sh
@@ -3,24 +3,25 @@
 export SLURM_TIME_FORMAT="%s"
 
 YEAR=2025
-partitions="all,cpu"
+partitions=""
+pp=${partitions:+"-r"}
 fields="jobid,user,cluster,account,partition,cputimeraw,elapsedraw,alloctres,start,submit,eligible,qos,state,jobname"
 cluster=della
 OUT=${cluster}.data.${YEAR}
 flags="-a -X -n -P"
 
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-01-01T00:00:00 -E ${YEAR}-01-31T23:59:59 -o ${fields} >  ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-02-01T00:00:00 -E ${YEAR}-03-01T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-03-02T00:00:00 -E ${YEAR}-03-31T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-04-01T00:00:00 -E ${YEAR}-04-30T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-05-01T00:00:00 -E ${YEAR}-05-31T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-06-01T00:00:00 -E ${YEAR}-06-30T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-07-01T00:00:00 -E ${YEAR}-07-31T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-08-01T00:00:00 -E ${YEAR}-08-31T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-09-01T00:00:00 -E ${YEAR}-09-30T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-10-01T00:00:00 -E ${YEAR}-10-31T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-11-01T00:00:00 -E ${YEAR}-11-30T23:59:59 -o ${fields} >> ${OUT}.raw
-sacct -M ${cluster} -r ${partitions} ${flags} -S ${YEAR}-12-01T00:00:00 -E ${YEAR}-12-31T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-01-01T00:00:00 -E ${YEAR}-01-31T23:59:59 -o ${fields} >  ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-02-01T00:00:00 -E ${YEAR}-03-01T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-03-02T00:00:00 -E ${YEAR}-03-31T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-04-01T00:00:00 -E ${YEAR}-04-30T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-05-01T00:00:00 -E ${YEAR}-05-31T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-06-01T00:00:00 -E ${YEAR}-06-30T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-07-01T00:00:00 -E ${YEAR}-07-31T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-08-01T00:00:00 -E ${YEAR}-08-31T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-09-01T00:00:00 -E ${YEAR}-09-30T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-10-01T00:00:00 -E ${YEAR}-10-31T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-11-01T00:00:00 -E ${YEAR}-11-30T23:59:59 -o ${fields} >> ${OUT}.raw
+sacct -M ${cluster} ${pp} ${partitions} ${flags} -S ${YEAR}-12-01T00:00:00 -E ${YEAR}-12-31T23:59:59 -o ${fields} >> ${OUT}.raw
 
 echo "Truncating job names that use the | character"
 wc ${OUT}.raw
